b2ab909d8ac8935fea232ea5d8240217474243ae
Wenszel <wiktor.smaga13@gmail.com>
2024-01-12 18:27:32 +0100

    Added pause/resume buttons

--- DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/SimulationView.java
+++ DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/SimulationView.java
@@ -14,10 +14,12 @@ import javafx.application.Platform;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -47,8 +49,10 @@ public class SimulationView implements SimulationListener {
     private VBox animalStatsBox;
     private Animal observedAnimal;
 
+    private Simulation simulation;
+
     public void init(SimulationConfig config) {
-        Simulation simulation = new Simulation(config);
+        simulation = new Simulation(config);
         simulation.addSubscriber(this);
         Thread simulationTask = new Thread(simulation);
         simulationTask.start();
@@ -160,5 +164,13 @@ public class SimulationView implements SimulationListener {
             }
         };
         Platform.runLater(task);
+    }
+
+    public void pause() {
+        simulation.pause();
+    }
+
+    public void resume() {
+        simulation.resume();
     }
 }
--- DarwinWorld/src/main/java/agh/ics/oop/Simulation.java
+++ DarwinWorld/src/main/java/agh/ics/oop/Simulation.java
@@ -16,21 +16,49 @@ public class Simulation implements Runnable {
     private final SimulationConfig config;
     private int dayCounter = 1;
     private double deadAnimalsAverageLifeLength;
+    private volatile boolean paused = false;
+    private final Object pauseLock = new Object();
+
     public Simulation(SimulationConfig config) {
         this.config = config;
         this.map = MapFactory.createMap(config);
+        map.initializeMap();
     }
+
     @Override
     public void run() {
-        map.initializeMap();
-        while(true) {
+        while (true) {
+            // Explanation of this code:
+            // https://stackoverflow.com/questions/2779484/why-must-wait-always-be-in-synchronized-block?noredirect=1&lq=1
+            synchronized (pauseLock) {
+                if (paused) {
+                    try {
+                        pauseLock.wait();
+                    } catch (InterruptedException ex) {
+                        break;
+                    }
+                }
+            }
             runDay();
             mapChanged("Day ended");
             try {
                 Thread.sleep(100);
-            } catch(InterruptedException err) {}
+            } catch(InterruptedException err) {
+                break;
+            }
         }
     }
+    public void pause() {
+        paused = true;
+    }
+
+    public void resume() {
+        synchronized (pauseLock) {
+            paused = false;
+            pauseLock.notifyAll();
+        }
+    }
+
     public void runDay() {
         deadAnimalsAverageLifeLength = map.removeDeadAnimals(dayCounter);
         map.moveAnimals();
