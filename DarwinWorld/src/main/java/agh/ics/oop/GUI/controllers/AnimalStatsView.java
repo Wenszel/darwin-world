3ec4626aa39d27543f2b226a3e0ead5068fcea9b
Wenszel <wiktor.smaga13@gmail.com>
2024-01-16 18:06:47 +0100

    Added stats tables

--- DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/AnimalStatsView.java
+++ DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/AnimalStatsView.java
@@ -2,34 +2,55 @@ package agh.ics.oop.GUI.controllers;
 
 import agh.ics.oop.model.mapElements.Animal;
 import agh.ics.oop.model.stats.AnimalStatistics;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class AnimalStatsView {
     private final VBox animalStatsBox;
+    private final TableView<Map.Entry<String, String>> tableView;
+    private static final String STATISTIC_COLUMN_NAME = "Statistic";
+    private static final String VALUE_COLUMN_NAME = "Value";
 
     public AnimalStatsView(VBox animalStatsBox) {
         this.animalStatsBox = animalStatsBox;
+
+        TableColumn<Map.Entry<String, String>, String> statNameColumn = new TableColumn<>(STATISTIC_COLUMN_NAME);
+        TableColumn<Map.Entry<String, String>, String> statValueColumn = new TableColumn<>(VALUE_COLUMN_NAME);
+        statNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getKey()));
+        statValueColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getValue()));
+
+        tableView = new TableView<>();
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        statNameColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        statValueColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        tableView.getColumns().addAll(statNameColumn, statValueColumn);
     }
 
     public void showAnimalStats(Animal animal) {
         animalStatsBox.getChildren().clear();
+
         AnimalStatistics stats = animal.getAnimalStats();
-        List<Text> statsFields = new LinkedList<>(
-                List.of(
-                        new Text("Genotype: " + stats.getGenotype()),
-                        new Text("Active gene: " + animal.getGenotype().getCurrentGene()),
-                        new Text("Energy: " + stats.getEnergy()),
-                        new Text("Eaten plants: " + stats.getEatenPlants()),
-                        new Text("Children amount: " + stats.getChildrenAmount()),
-                        new Text("Descendant amount: " + stats.getDescendantAmount()),
-                        new Text("Day alive: " + stats.getDayAlive()),
-                        new Text("Death day: " + stats.getDeathDay())
-                )
-        );
-        animalStatsBox.getChildren().addAll(statsFields);
+        Map<String, String> statsMap = new LinkedHashMap<>();
+
+        statsMap.put("Genotype", stats.getGenotype().toString());
+        statsMap.put("Active gene", String.valueOf(animal.getGenotype().getCurrentGene()));
+        statsMap.put("Energy", String.valueOf(stats.getEnergy()));
+        statsMap.put("Eaten plants", String.valueOf(stats.getEatenPlants()));
+        statsMap.put("Children amount", String.valueOf(stats.getChildrenAmount()));
+        statsMap.put("Descendant amount", String.valueOf(stats.getDescendantAmount()));
+        statsMap.put("Day alive", String.valueOf(stats.getDayAlive()));
+        statsMap.put("Death day", stats.getDeathDay() == 0 ? "Still alive" : String.valueOf(stats.getDeathDay()));
+
+        ObservableList<Map.Entry<String, String>> items = FXCollections.observableArrayList(statsMap.entrySet());
+        tableView.setItems(items);
+
+        animalStatsBox.getChildren().add(tableView);
     }
 }
--- DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/SimulationStatsView.java
+++ DarwinWorld/src/main/java/agh/ics/oop/GUI/controllers/SimulationStatsView.java
@@ -1,31 +1,67 @@
 package agh.ics.oop.GUI.controllers;
 
 import agh.ics.oop.model.stats.SimulationStatistics;
-import agh.ics.oop.model.stats.StatsFileManager;
+import agh.ics.oop.model.utils.Genotype;
+import agh.ics.oop.model.utils.KeyValue;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SimulationStatsView {
     private final VBox simulationStatsBox;
+    private final TableView<Map.Entry<String, String>> tableView;
+    private static final String STATISTIC_COLUMN_NAME = "Statistic";
+    private static final String VALUE_COLUMN_NAME = "Value";
 
     public SimulationStatsView(VBox simulationStatsBox) {
         this.simulationStatsBox = simulationStatsBox;
+
+        TableColumn<Map.Entry<String, String>, String> statNameColumn = new TableColumn<>(STATISTIC_COLUMN_NAME);
+        TableColumn<Map.Entry<String, String>, String> statValueColumn = new TableColumn<>(VALUE_COLUMN_NAME);
+        statNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getKey()));
+        statValueColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getValue()));
+
+        tableView = new TableView<>();
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        statNameColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        statValueColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        tableView.getColumns().addAll(statNameColumn, statValueColumn);
     }
 
     public void showSimulationStats(SimulationStatistics stats) {
         simulationStatsBox.getChildren().clear();
-        List<Text> statsFields = new LinkedList<>(List.of(
-                new Text("Day: " + stats.getCurrentDay()),
-                new Text("Animals amount: " + stats.getAnimalsAmount()),
-                new Text("Plants amount: " + stats.getPlantsAmount()),
-                new Text("Empty fields: " + stats.getEmptyFieldsAmount()),
-                new Text("Most popular genotypes: " + stats.getMostPopularGenotypes()),
-                new Text("Average energy: " + stats.getAverageEnergy()),
-                new Text("Average alive animals children: " + stats.getAverageAnimalsChildrenAmount()),
-                new Text("Average dead animals life length: " + stats.getAverageDeadAnimalsLifeLength())));
-        simulationStatsBox.getChildren().addAll(statsFields);
+
+        Map<String, String> statsMap = new LinkedHashMap<>();
+        statsMap.put("Day", String.valueOf(stats.getCurrentDay()));
+        statsMap.put("Animals amount", String.valueOf(stats.getAnimalsAmount()));
+        statsMap.put("Plants amount", String.valueOf(stats.getPlantsAmount()));
+        statsMap.put("Empty fields", String.valueOf(stats.getEmptyFieldsAmount()));
+        statsMap.put("Most popular genotypes", formatGenotypes(stats.getMostPopularGenotypes()));
+        statsMap.put("Average energy", formatDouble(stats.getAverageEnergy()));
+        statsMap.put("Average alive animals children", formatDouble(stats.getAverageAnimalsChildrenAmount()));
+        statsMap.put("Average dead animals life length", formatDouble(stats.getAverageDeadAnimalsLifeLength()));
+        ObservableList<Map.Entry<String, String>> items = FXCollections.observableArrayList(statsMap.entrySet());
+        tableView.setItems(items);
+
+        simulationStatsBox.getChildren().add(tableView);
+    }
+
+    private String formatGenotypes(List<KeyValue<Genotype, Integer>> genotypes) {
+        StringBuilder result = new StringBuilder();
+        for (KeyValue<Genotype, Integer> genotype : genotypes) {
+            result.append(genotype.getKey()).append(" (").append(genotype.getValue()).append(") \n");
+        }
+        return result.toString();
+    }
+
+    private String formatDouble(double number) {
+        return String.format("%.2f", number);
     }
 }
